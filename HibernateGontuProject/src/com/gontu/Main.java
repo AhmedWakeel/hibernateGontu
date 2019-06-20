17aff04a85c1876818c161ab6dab068e87a81a09
PLM-AHMEDW\Admin <ahmed@pragiti.com>
2019-06-20 18:40:25 +0530

    add

--- HibernateGontuProject/src/com/gontu/Main.java
+++ HibernateGontuProject/src/com/gontu/Main.java
@@ -1,7 +1,5 @@
 package com.gontu;
 
-import java.util.Date;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -13,14 +11,8 @@ public class Main {
 	{
 		
 		Student  student = new Student();
-		student.setDate(new Date());
-		student.setName("mk44");
 		
-		Student_Detail detail = new Student_Detail();
-		detail.setStudent_mobile_number("990998");
-		detail.setStudent(student);
-		
-		student.setDetail(detail);
+	
 		
 		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
